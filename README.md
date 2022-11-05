f475d754e81f0c9cde1507267784a2d5ca1f2327
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-11-05 15:42:25 +0800

    env settings

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cozies-image-generation
+
+### Checks
+- If there is a None Start From 00
+
+Property Name Comments
+- 是否要加上顏色
+- ＋ 的格式統一
+
+- Check Property Count and Names
+
+No Duplicate
+- G, Top
+- I, Backpack
+- J, Hair
+
+### Resource
+- [Check list of lists duplicate](https://tutorial.eyehunts.com/python/python-find-duplicates-in-list-of-lists-example-code/)
---
+++ src/env.py
@@ -0,0 +1,44 @@
+logFile = "log/propRecord-2022-11-05-00-29.csv"
+
+ticketName = "Diamond Ticket"
+ticketDesc = "Cut (Burn) this Diamond Ticket to reveal your Cozies Identity (NFT)"
+ticketImg = "ipfs://QmSGrKzTKLm9q2oZfvgzAENcZqBfwXExKpXmxn2NGmyWiM"
+ticketAni = "ipfs://QmeT4Hj1sNcD4qtCb412qBTasp8coAfqtfpyr79Y7QCuCB"
+
+seriesName = "Cozies"
+description = "A collection of 10,000 unique characters, Cozies is an immersive community-driven movement focused on a \"cozy\" culture of self-exploration and well-being. By owning a Cozies NFT, you are making a statement that you are comfortable with who you are, what you own, and where you are going. Your Cozies NFT is the key to this journey. \n \n Happiness is a choice, and it starts with feeling Cozy."
+ipfsBaseURI = "https://cozies.s3.ap-southeast-1.amazonaws.com/nft/images/Cozies_"
+arBaseURI = "https://cozies.s3.ap-southeast-1.amazonaws.com/nft/images/Cozies_"
+
+logResultPath = "log"
+imgResultPath = "out/img"
+jsonResultPath = "out/json"
+propertiesPath = "src/assets/properties"
+
+backgroundColorPath = "A. Background"
+backgroundPatternPath = "B. Journey"
+bodyPath = "C. Aura"
+skinPath = "D. Scars"
+shoePath = "E. Shoe"
+lowerBodyPath = "F. Legs"
+upperBodyPath = "G. Top"
+handPath = "H. Hand"
+backpackPath = "I. Backpack"
+hairPath = "J. Hair"
+neckPath = "K. Fren"
+headdress = "L. Head"
+
+propertiesListInOrder = {
+    "A": backgroundColorPath,
+    "B": backgroundPatternPath,
+    "C": bodyPath,
+    "D": skinPath,
+    "E": shoePath,
+    "F": lowerBodyPath,
+    "G": upperBodyPath,
+    "H": handPath,
+    "I": backpackPath,
+    "J": hairPath,
+    "K": neckPath,
+    "L": headdress
+}
